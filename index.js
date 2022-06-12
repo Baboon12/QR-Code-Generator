62b0edd331b9d833983bfe1b00e240aec124a06e
Bhavya Sura <bhavyasura12@gmail.com>
2022-06-12 12:12:06 +0530

    Commit 2

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post("/scan", (req, res) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Server up and Running at ", port);
 });
